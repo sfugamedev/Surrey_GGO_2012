a2b2aa5c92a62c962158634d31c7857fdb6406d5
lrd <perrytan68@gmail.com>
2012-11-13 09:57:39 -0800

    Add project framework

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Character : GDCObject {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
